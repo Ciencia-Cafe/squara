057cb9a515a4dbc780d853dcce57ce991c7ee46d
Walle <wllabferreira@gmail.com>
2025-07-10 10:59:03 -0300

    Change Tokenizer

--- parse.c
+++ parse.c
@@ -12,24 +12,22 @@ typedef struct {
     char *raw_str;
     char *head;
     int backend;
-    Stack input;
-    Stack output;
-    Stack opstack;
+    Stack tokens;
+    // Stack output;
+    // Stack opstack;
 } Lexer;
 
 typedef struct {
     int type;
-    char *head;
-    char *tail;
-    bool assoc;
-    int precedence;
+    int size;
+    char *lexeme;
 } Token;
 
 typedef enum {
     TOKEN_UNKNOWN,
     TOKEN_NUMBER,
     TOKEN_VARIABLE,
-    TOKEN_OPERATOR,
+    TOKEN_BINARY_OPERATOR,
     TOKEN_FUNCTION
 } TokenTypes;
 
@@ -39,8 +37,8 @@ typedef enum {
 } LexerBackends;
 
 typedef enum {
-    TOKEN_OK,
-    TOKEN_EXTRA_DOT
+    TOKENIZER_STATE_OK,
+    TOKENIZER_STATE_INVALID,
 } TokenErrors;
 
 static const OperatorPrecedence g_OpPrec[] = {
@@ -55,132 +53,107 @@ Lexer LexerInit(char *expr, int backend) {
         .raw_str = expr,
         .head = expr,
         .backend = backend,
-        .input = StackInit(100, sizeof(Token)),
-        .output = StackInit(100, sizeof(Token)),
-        .opstack = StackInit(100, sizeof(Token))
+        .tokens = StackInit(100, sizeof(Token)),
+        // .output = StackInit(100, sizeof(Token)),
+        // .opstack = StackInit(100, sizeof(Token))
     };
     return lexer;
 }
 
 void LexerFree(Lexer *lex) {
-    StackFree(&lex->input);
-    StackFree(&lex->output);
-    StackFree(&lex->opstack);
+    StackFree(&lex->tokens);
+    // StackFree(&lex->output);
+    // StackFree(&lex->opstack);
 }
 
 int Tokenize(Lexer *lex) {
+    Token t = {0};
 
-    char *end = lex->raw_str + strlen(lex->raw_str);
+    char *head = lex->raw_str;
+    char *start = head;
     
-    while(lex->head < end) {
-		while(
-			lex->head[0] == ' '	    ||
-			lex->head[0] == '\n'	||
-			lex->head[0] == '\t'	||
-			lex->head[0] == '\r'    ||
-			lex->head[0] == '\v'
-		) lex->head++;
+    while(*head) {
+        switch (*head) {
+        case '\0':
+            break;
+        case ' ':
+        case '\n':
+        case '\t':
+        case '\r':
+        case '\v':
+            head++;
+            break;
 
-		if(lex->head[0] == '\0') break;
+        // Operators
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+            t.lexeme = head;
+            t.size = 1;
+            t.type = TOKEN_BINARY_OPERATOR;
+            StackPush(&lex->tokens, &t);
+            head++;
+            break;
 
-        Token t = {0};
-        
-        // numbers starting with .
-		if(lex->head[0] == '.') {
-            t.head = lex->head;
-            do {
-                ++lex->head;
-                if(lex->head[0] == '.') return TOKEN_EXTRA_DOT;
-            } while(lex->head[0] >= '0' && lex->head[0] <= '9');
-
-		    t.type = TOKEN_NUMBER;
-		    t.tail = lex->head;
-		    StackPush(&lex->input, &t);
-		    continue;
-		}
-
-		// numbers starting with numbers
-		if(lex->head[0] >= '0' && lex->head[0] <= '9') {
-            t.head = lex->head;
-            bool dot_seen = false;
-            do {
-                ++lex->head;
-                if(dot_seen == true && lex->head[0] == '.') {
-                    return TOKEN_EXTRA_DOT;
-                }
-                if(lex->head[0] == '.') {
-                    dot_seen = true;
-                }
-            } while((lex->head[0] >= '0') && (lex->head[0] <= '9') || (lex->head[0] == '.'));
+        // Numbers
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            t.lexeme = head;
+            start = head;
             t.type = TOKEN_NUMBER;
-            t.tail = lex->head;
-            StackPush(&lex->input, &t);
-            continue;
-		}
+            
+            while (head[0] >= '0' && head[0] <= '9') ++head;
+            if (head[0] == '.') ++head;
+            while (head[0] >= '0' && head[0] <= '9') ++head;
 
-		// variables
-		if(
-		    (lex->head[0] >= 'A' && lex->head[0] <= 'Z') ||
-		    (lex->head[0] >= 'a' && lex->head[0] <= 'z') ||
-		    lex->head[0] == '_') {
-		    t.head = lex->head;
-		    do {
-		        ++lex->head;
-		    } while (
-				(lex->head[0] >= 'A' && lex->head[0] <= 'Z') ||
-				(lex->head[0] >= 'a' && lex->head[0] <= 'z') ||
-				(lex->head[0] >= '0' && lex->head[0] <= '9') ||
-				lex->head[0] == '_');
+            t.size = head-start;
+            StackPush(&lex->tokens, &t);
+            break;
+        default:
+            // Names
+            if(
+                (head[0] >= 'A' && head[0] <= 'Z') ||
+                (head[0] >= 'a' && head[0] <= 'z') ||
+                head[0] == '_')
+            {
+                t.lexeme = head;
+                start = head;
+                do {
+                    ++head;
+                } while (
+                    (head[0] >= 'A' && head[0] <= 'Z') ||
+                    (head[0] >= 'a' && head[0] <= 'z') ||
+                    (head[0] >= '0' && head[0] <= '9') ||
+                    head[0] == '_');
+                t.type = TOKEN_VARIABLE;
+                t.size = head - start;
+                StackPush(&lex->tokens, &t);
+                break;
+            }
 
-			t.type = TOKEN_VARIABLE;
-			t.tail = lex->head;
-			StackPush(&lex->input, &t);
-			continue;
-		}
-
-		// operators
-		switch(lex->head[0]) {
-		    case '/':
-		        t.precedence = 3;
-		        break;
-		    case '*':
-		        t.precedence = 3;
-		        break;
-		    case '-':
-		        t.precedence = 2;
-		        break;
-		    case '+':
-		        t.precedence = 2;
-		        break;
-		}
-		if(t.precedence) {
-		    t.head = lex->head;
-		    t.tail = lex->head;
-            t.type = TOKEN_OPERATOR;
-
-            StackPush(&lex->input, &t);
-            lex->head++;
-		}
-
+            // Invalid token
+            lex->head = head;
+            return TOKENIZER_STATE_INVALID;
+        }
     }
+
+    return TOKENIZER_STATE_OK;
 }
 
 void TokenPrint(Token t) {
-    for(int i = 0; &t.head[i] <= &t.tail[0]; i++){
-        printf("%c", t.head[i]);
+    for(int i = 0; i <= t.size; i++){
+        printf("%c", t.lexeme[i]);
     }
     printf("\n");
 }
 
 void TokenStackPrint(Lexer *lex) {
-    printf("Input buffer size: %lu\n", lex->input.head);
+    printf("Input buffer size: %lu\n", lex->tokens.head);
 
-    Token *t = (Token*) lex->input.buffer;
-    for(int i = 0; i < lex->input.head; i++){
+    Token *t = (Token*) lex->tokens.buffer;
+    for(int i = 0; i < lex->tokens.head; i++){
         TokenPrint(t[i]);
-        // for(int j = 0; &t[i].head[j] <= t[i].tail; j++){
-        //     printf("%c", t[i].head[j]);
-        // }
     }
 }
 
